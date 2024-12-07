b1a05758474348feadcf9bb532078b05bd5c193d
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2024-12-07 13:54:47 -0300

    Maior e menor numero em lista V2

--- Atividade em Python/lista+-pos.py
+++ Atividade em Python/lista+-pos.py
@@ -1,17 +1,27 @@
 num =[]
 for pos in range(0,5):
     num.append(int(input("Digite os valores: ")))
-print(f"{num}")
+print(f"Os números presentes são {num}")
 
 countmax=list()
 countmin=list()
-
+igual=list()
 
 for c,n in enumerate(num):
     if n == max(num):
         countmax.append(c)
     elif n == min(num):
         countmin.append(c)
-print(f"O maior número foi {max(num)} nas posições {countmax}")
-print(f"O maior número foi {min(num)} nas posições {countmin}")
+
+if max(num) != min(num):
+    print(f"O maior número foi {max(num)} nas posições ",end='')
+    for l in countmax:
+        print(f"{l}",end="... ")
+    print(f"\nO menor número foi {min(num)} nas posições ",end="")
+    for m in countmin:
+        print(f"{m}",end='... ')
+else:
+    print(f"O maior e menor número foi {n} e ele estava em todas as posiçôes : ",end="")
+    for i in countmax:
+        print(f'{i}',end='... ')
 
