93df785356922eb51b7c85825449ec4c9a03b5f1
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2024-12-14 09:06:54 -0300

    Área função
    Um programa pequeno feito para testar funções

---
+++ Atividade em Python/área_func.py
@@ -0,0 +1,20 @@
+def area(a ,b):
+    area = a * b
+    print(f'A área de um terreno {a:.2f} X {b:.2f} é de {area:.2f}m²')
+
+print('<<<   Controle de área   >>>')
+while True:
+    a = float(input('Digite o comprimento: '))
+    b = float(input('Digite a Largura: '))
+    area(a,b)
+    print('-='*25)
+    while True:
+        esc = str(input('Deseja continuar[S/N]: ')).strip().upper()[0]
+        if esc in 'NS':
+            break
+        else:
+            print('Tente Novamente. ',end='')
+    if esc in 'N':
+        break
+print('-='*25)
+print('<<<    ENCERRADO    >>>>')
