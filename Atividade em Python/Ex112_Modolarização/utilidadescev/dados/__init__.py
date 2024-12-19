4b83b2329e3cf855e2e37fd0dc03cd7c636962c8
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2024-12-19 08:35:47 -0300

    acrescentando funções em dados

--- Atividade em Python/Ex112_Modolarização/utilidadescev/dados/__init__.py
+++ Atividade em Python/Ex112_Modolarização/utilidadescev/dados/__init__.py
@@ -7,3 +7,23 @@ def leiadin():
         except:
             print(f'\033[31mERRO! "{dinheiro}" é um preço invalido\033[m')
     return dinheiro
+
+def leiaint():
+    while True:
+        r = str(input('Digite um inteiro: '))
+        try:    
+            int(r)
+            break
+        except:
+            print(f'\033[31mErro! {r} não é um inteiro valido\033[m')
+    return r
+
+def leiafloat():
+    while True:
+        f = str(input('Digite um número real: '))
+        try:
+            float(f)
+            break
+        except:
+            print(f'\033[31mErro! {f} não é um inteiro valido\033[m')
+    return f
