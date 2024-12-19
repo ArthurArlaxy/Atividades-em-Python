765cd7eb3d63e4199093d4513bfdebf2732936c1
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2024-12-19 08:35:05 -0300

    leia int e float com tratamento
    Leia números inteiro e reais com uma função que possui tratamento de erros

---
+++ Atividade em Python/Ex113.py
@@ -0,0 +1,32 @@
+def leiaint():
+    while True:
+        try:    
+            r = str(input('Digite um inteiro: ')).strip()
+            int(r)
+            break
+        except KeyboardInterrupt:
+            print(f'\n\033[31mO usuário não digitou\033[m')
+            r= 'nenhum'
+            return 'nenhum'
+        except:
+            print(f'\033[31mErro! "{r}" não é um inteiro valido\033[m')
+    return r
+
+def leiafloat():
+    while True:
+        try:
+            f = str(input('Digite um número real: ')).strip()
+            float(f)
+            break
+        except KeyboardInterrupt:
+            print('\n\033[31mO usuário não digitou\033[m')
+            return 'nenhum'
+        except :
+            print(f'\033[31mErro! "{f}" não é um real valido\033[m')
+    return f
+
+
+inteiro = leiaint()
+real = leiafloat()
+print('~'*100)
+print(f'O número inteiro digitado foi {inteiro} e o real foi {real}')
