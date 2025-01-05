b5e62508f1df63c80d9e64107f96a3e02a5e9cee
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-01-04 21:50:56 -0300

    Alunos, banco de dados POO
    Criação de um sistema  com classes e objetos que cria põe alunos em um banco de dados

---
+++ Atividade em Python/aluno_POO.py
@@ -0,0 +1,33 @@
+class Aluno():
+    def __init__(self):
+        self.nome  =''
+        self.idade = 0
+        self.turma = ''
+    
+    def pegar_info(self):
+        self.nome = str(input('NOME:'))
+        self.idade = int(input('IDADE:'))
+        self.turma = str(input('Turma'))
+
+class BancoDeDados():
+    def __init__(self):
+        self.base = {}
+        self.contador = 0
+
+    def adicionar(self, aluno):
+        self.contador += 1
+        self.base[self.contador] = aluno
+    
+    def listar(self):
+        for id, aluno in self.base.items():
+            print(f"ID: {id}, Nome: {aluno.nome}, Idade: {aluno.idade}, Turma: {aluno.turma}")
+
+banco = BancoDeDados()
+aluno1 = Aluno()
+aluno1.pegar_info()
+banco.adicionar(aluno1)
+banco.listar()
+aluno2 = Aluno()
+aluno2.pegar_info()
+banco.adicionar(aluno2)
+banco.listar()
