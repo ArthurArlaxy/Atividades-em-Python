e5742eef5e740a94171c25a179ff1e68bba516e0
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-01-13 08:04:46 -0300

    input (OneBitCode)

---
+++ Atividade em Python/3-input.py
@@ -0,0 +1,11 @@
+# Utilizando o Input
+
+name = input('Digite o nome: ')
+yearLaunch = int(input('Digite o ano: '))
+gamePrice = float(input('Digite o preço\n'))
+planIncluded = (input('Digite se está incluso: '))
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planIncluded)
