16c3116c088c31dbaadd9ae679704eaaa7c8883d
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-01-14 08:43:45 -0300

    Strings (OneBitCodr)

---
+++ Atividade em Python/6-strings.py
@@ -0,0 +1,10 @@
+gameName1 = 'Fifa'
+gameName2 = "fifa"
+print(gameName1)
+print(gameName2)
+print(gameName1 == gameName2)
+gameDescription = """
+Fifa é um jogo de Futebol desenvolvido pela EA Sports
+Jogo Mercenário que possibilita jogar localmente ou online
+"""
+print(gameDescription)
