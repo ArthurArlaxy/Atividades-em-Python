fa82e4eb1751db740b790a64b7d97e89635b4986
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-01-14 08:44:58 -0300

    Métodos Strings (OneBitCode)

---
+++ Atividade em Python/9-métodos_string.py
@@ -0,0 +1,14 @@
+
+gameDescription = """
+Fifa é um jogo de Futebol, desenvolvido pela EA Sports,
+jogo Mercenário, possibilita jogar localmente ou online
+"""
+gameName = "Fifa 25 -  EA Sports"
+
+print(gameName.upper())#passar para maiúsculo
+print(gameName.lower())#passar para minúsculo
+print(gameName.capitalize()) # Apenas a primeira letra em maiúsculo
+print(gameName.title()) # apenas a primeira letra em cada palavra em maiúsculo
+print(gameDescription.count('jogo'))
+print(gameDescription.split(','))
+
