32505ce30c11bc909d532730f2b3e4e0f1ba2133
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-01-15 17:53:44 -0300

    One oracle

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,6 @@
 alert('Boas Vindas ao jogo do número secreto');
 let secretNumber = 10;
-prompt('Escolha um número entre 1 e 30');
+let number = prompt('Escolha um número entre 1 e 30');
+if (secretNumber == number){
+    console.log('Isso você acertou o número secreto ')
+};
