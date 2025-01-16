271875205cf07acc60f76b98b693f092f4534823
Arthur Albuquerque <arthuraadeoliveira@gmail.com>
2025-01-16 11:44:51 -0300

    Exercício Final Fundamentos OneBitCode

---
+++ Atividade em Python/ex-fim1-onebitcode.py
@@ -0,0 +1,56 @@
+def listar(times):
+    for n,time in enumerate(times):
+        for nome,elenco in time.items():
+            print(f'O time {nome} tem {len(elenco)} jogadores cadastratados e seu indice é {n}')
+
+def addTime():
+    listar(times)
+    time = input('Digite o nome do time que deseja adicionar: ').strip()
+    times.append({time: []})
+
+def removerTime():
+    listar(times)
+    rem = int(input('Digite o id do time que deseja remover: '))
+    if rem < len(times) and rem >=0 :
+        times.pop(rem)
+        print('Time removido')
+    else:
+        print('O ID inserido não existe')
+
+def addJogador():
+    listar(times)
+    id = int(input('Digite o id do time que deseja adicionar o jogador: '))
+    if id < len(times) and id >= 0:
+        jogador = input('Digite o nome do jogador que deseja adicionar: ').strip()
+        nomeTime = list(times[id].keys())[0]
+        times[id][nomeTime].append(jogador)
+    else:
+        print('Time não existe')
+
+def removeJogador():
+    listar(times)
+    id = int(input('Digite o id do time que deseja remover o jogador: '))
+    jogador = input('Digite o nome do jogador que deseja remover: ').strip()
+    nomeTime = list(times[id].keys())[0]
+    if jogador in times[id][nomeTime]:
+        times[id][nomeTime].remove(jogador)
+    else:
+        print(f'O jogador {jogador} não existe no time {nomeTime}')
+
+def listarJogadores():
+    id = int(input('Digite o id do time que deseja saber os jogadores: '))
+    if id < len(times) and id >= 0:
+        nomeTime = list(times[id].keys())[0]
+        print(f'Os jogadores presentes no time {nomeTime} são {times[id][nomeTime]}')
+
+times = [
+    {'Flamengo': ['Arrascaeta', 'Nico', 'Pedro', 'Cebolinha','beijinho']},
+    {'Fluminense': ['Cano', 'John Arias']},
+    {'Barcelona': ['Lamine','lewa', 'raphinha']}
+]
+
+listar(times)
+addTime()
+removerTime()
+addJogador()
+removeJogador()
